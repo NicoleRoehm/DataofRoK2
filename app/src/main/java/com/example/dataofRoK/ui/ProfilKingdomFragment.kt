e9adbe6d29a4caf808b38276fc31d7e605504f22
NicoleRöhm <donjay1014@gmail.com>
2023-04-20 18:32:04 +0200

    DataofRok

--- app/src/main/java/com/example/dataofRoK/ui/ProfilKingdomFragment.kt
+++ app/src/main/java/com/example/dataofRoK/ui/ProfilKingdomFragment.kt
@@ -1,6 +1,46 @@
 package com.example.dataofRoK.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.example.DataofRoK.databinding.FragmentKingdomsBinding
+import com.example.dataofRoK.KingdomViewModel
+
 
 class ProfilKingdomFragment : Fragment() {
+
+    private var _binding: FragmentKingdomsBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: KingdomViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentKingdomsBinding.inflate(inflater,container,false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
+    }
+
 }
