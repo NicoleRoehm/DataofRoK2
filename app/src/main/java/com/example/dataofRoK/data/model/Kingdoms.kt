404404d80e1c8d9a7f69c0681d5b5c969887259f
NicoleRöhm <donjay1014@gmail.com>
2023-04-20 18:19:34 +0200

    DataofRok

--- app/src/main/java/com/example/dataofRoK/data/model/Kingdoms.kt
+++ app/src/main/java/com/example/dataofRoK/data/model/Kingdoms.kt
@@ -1,3 +1,5 @@
 package com.example.dataofRoK.data.model
 
-data class Kingdoms (val name : String, val number : Int)
+data class Kingdoms (val name : String, val number : Int, val kills: Long, val killpoints: Long,
+                     val tFourkills: Long, val tFivekills: Long, val energyKd: Long,
+                     val deathKd: Long, val kingname: String, val mainAlliance: String)
---
+++ app/src/main/java/com/example/dataofRoK/ui/ProfilKingdomFragment.kt
@@ -0,0 +1,6 @@
+package com.example.dataofRoK.ui
+
+import androidx.fragment.app.Fragment
+
+class ProfilKingdomFragment : Fragment() {
+}
